ca76a0f7b59a7b24599ef1b828fc52ea8531255e
VincenzoMarcello <vincenzo.marcello.92@gmail.com>
2023-09-12 18:33:39 +0200

    feat: add Component CardList

--- src/components/Cards/CardsList.vue
+++ src/components/Cards/CardsList.vue
@@ -13,6 +13,7 @@ export default {
       axios
         .get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0")
         .then((response) => {
+          this.cards = response.data.data;
           console.log(response.data.data);
         });
     },
@@ -24,6 +25,35 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <div class="container">
+    <div class="row g-4 row-cols-2 row-cols-md-3 row-cols-lg-5 my-5">
+      <div class="col" v-for="card in cards" :key="card.id">
+        <img v-for="image of card.card_images" :src="image.image_url" alt="" />
+        <p>{{ card.name }}</p>
+        <p>{{ card.type }}</p>
+      </div>
+    </div>
+  </div>
+</template>
 
-<style></style>
+<style lang="scss" scoped>
+img {
+  max-width: 100%;
+}
+p {
+  text-align: center;
+  font-weight: 500;
+  font-size: 0.8rem;
+
+  margin-top: 5px;
+  margin-bottom: 0;
+  padding: 1rem;
+
+  border: 1px solid black;
+  border-radius: 5px;
+
+  background-color: antiquewhite;
+  cursor: pointer;
+}
+</style>
