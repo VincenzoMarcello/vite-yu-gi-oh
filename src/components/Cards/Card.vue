e12324d98dd89ef38dd70f2f3632ee7631ab4898
VincenzoMarcello <vincenzo.marcello.92@gmail.com>
2023-09-13 16:46:19 +0200

    feat: add props

---
+++ src/components/Cards/Card.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  props: {
+    image_url: String,
+    name: String,
+    type: String,
+  },
+};
+</script>
+
+<template>
+  <div class="col">
+    <img :src="image_url" alt="" />
+    <p>{{ name }}</p>
+    <p>{{ type }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+img {
+  max-width: 100%;
+}
+p {
+  text-align: center;
+  font-weight: 500;
+  font-size: 0.8rem;
+
+  margin-top: 5px;
+  margin-bottom: 0;
+  padding: 1rem;
+
+  border: 1px solid black;
+  border-radius: 5px;
+
+  background-color: rgba(250, 235, 215, 0.9);
+  cursor: pointer;
+}
+</style>
--- src/components/Cards/CardsList.vue
+++ src/components/Cards/CardsList.vue
@@ -1,5 +1,6 @@
 <script>
 import axios from "axios";
+import Card from "./Card.vue";
 
 export default {
   data() {
@@ -7,6 +8,8 @@ export default {
       cards: [],
     };
   },
+
+  components: { Card },
 
   methods: {
     fetchCards() {
@@ -28,32 +31,23 @@ export default {
 <template>
   <div class="container">
     <div class="row g-4 row-cols-2 row-cols-md-3 row-cols-lg-5 my-5">
-      <div class="col" v-for="card in cards" :key="card.id">
-        <img v-for="image of card.card_images" :src="image.image_url" alt="" />
+      <!-- <div class="col" v-for="card in cards" :key="card.id">
+        <img :src="card.card_images[0].image_url" alt="" />
         <p>{{ card.name }}</p>
         <p>{{ card.type }}</p>
-      </div>
+      </div> -->
+
+      <!-- STAMPA COMPONENTE CARD CON V-FOR DEL COMPONENTE -->
+
+      <Card
+        v-for="card in cards"
+        :key="card.id"
+        :image_url="card.card_images[0].image_url"
+        :name="card.name"
+        :type="card.type"
+      />
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
-img {
-  max-width: 100%;
-}
-p {
-  text-align: center;
-  font-weight: 500;
-  font-size: 0.8rem;
-
-  margin-top: 5px;
-  margin-bottom: 0;
-  padding: 1rem;
-
-  border: 1px solid black;
-  border-radius: 5px;
-
-  background-color: antiquewhite;
-  cursor: pointer;
-}
-</style>
+<style lang="scss" scoped></style>
