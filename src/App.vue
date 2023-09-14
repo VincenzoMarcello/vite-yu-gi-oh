66515fae2edf22c44a5f06caa7a51bb8d6e8c38f
VincenzoMarcello <vincenzo.marcello.92@gmail.com>
2023-09-14 13:08:37 +0200

    feat: add ex complete

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
 import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
 import CardList from "./components/Cards/CardsList.vue";
-import BaseSelect from "./components/Select/BaseSelect.vue";
 
 export default {
   data() {
@@ -12,28 +11,12 @@ export default {
       apiYu: "https://db.ygoprodeck.com/api/v7",
     };
   },
-  components: { AppHeader, CardList, BaseSelect },
-
-  methods: {
-    search(selectedType) {
-      const fullApiYu = `${this.apiYu}/cardinfo.php?archetype=${selectedType}`;
-      console.log(fullApiYu);
-    },
-
-    // ???
-    fetchArch(fullApiYu) {
-      // axios.get(fullApiYu).then((response) => {
-      //   this.archetypes = response.data;
-      //   console.log(response.data);
-      // });
-    },
-  },
+  components: { AppHeader, CardList },
 };
 </script>
 
 <template>
   <AppHeader />
-  <BaseSelect @archtype-serch="search" />
   <CardList />
 </template>
 
--- src/components/Cards/CardsList.vue
+++ src/components/Cards/CardsList.vue
@@ -1,15 +1,17 @@
 <script>
 import axios from "axios";
 import Card from "./Card.vue";
+import BaseSelect from "../Select/BaseSelect.vue";
 
 export default {
   data() {
     return {
       cards: [],
+      apiYu: "https://db.ygoprodeck.com/api/v7",
     };
   },
 
-  components: { Card },
+  components: { Card, BaseSelect },
 
   methods: {
     fetchCards() {
@@ -19,6 +21,15 @@ export default {
           this.cards = response.data.data;
           console.log(response.data.data);
         });
+    },
+
+    search(selectedType) {
+      const fullApiYu = `${this.apiYu}/cardinfo.php?archetype=${selectedType}`;
+
+      axios.get(fullApiYu).then((response) => {
+        this.cards = response.data.data;
+        console.log(response.data.data);
+      });
     },
   },
 
@@ -30,6 +41,8 @@ export default {
 
 <template>
   <div class="container">
+    <BaseSelect @archtype-serch="search" />
+
     <div class="row g-4 row-cols-2 row-cols-md-3 row-cols-lg-5 my-5">
       <!-- <div class="col" v-for="card in cards" :key="card.id">
         <img :src="card.card_images[0].image_url" alt="" />
